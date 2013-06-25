91bd90e999fad7beebfa465797f80111ef1b4572
Wraithan (Chris McDonald) <xwraithanx@gmail.com>
2013-06-25 15:30:11 -0700

    init

---
+++ README.rst
@@ -0,0 +1,6 @@
+ZenIRCBot MyFitnessPal
+======================
+
+Lets ZenIRCBot_ look up your information on MyFitnessPal
+
+.. _ZenIRCBot: https://github.com/zenirc/zenircbot
---
+++ myfitnesspal.js
@@ -0,0 +1,42 @@
+var ZenIRCBot = require('zenircbot-api').ZenIRCBot
+  , scrapeit = require('scrapeit')
+  , zen = new ZenIRCBot()
+
+zen.register_commands(
+  "myfitnesspal.js"
+, [{
+    name: "!calories"
+  , description: "Lets you know your calories so far."
+  }]
+)
+
+var url = 'http://www.myfitnesspal.com/food/diary/'
+  , user = 'Wraithan'
+  , fullUrl = url + user
+
+
+var filtered = zen.filter({version: 1, type: 'directed_privmsg'})
+filtered.on('data', function(msg){
+  if (/^calories$/i.test(msg.data.message)) {
+    scrapeit(fullUrl, function(err, o, dom) {
+      if (!err) {
+        var tags = ['Today', 'Goal', 'Remaining']
+        o('tr.total').forEach(function(tr, idx) {
+          var count = 0
+          tr.children.forEach(function(td) {
+            if (count < 2) {
+              if (td.type === 'tag' && td.name === 'td') {
+                if (count === 1) {
+                  zen.send_privmsg(msg.data.channel,
+                                   tags[idx] + ': ' + td.children[0].data.trim())
+                  console.log(td)
+                }
+                count++
+              }
+            }
+          })
+        })
+      }
+    })
+  }
+})
